562d1140ba3f22636d0c8d7fe3fb306751ffbd07
alieffe <119794592+alieffe@users.noreply.github.com>
2025-06-25 11:31:53 +0330

    user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -4,26 +4,27 @@
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
     "https://t.me/s/v2rayfree",
-    "https://t.me/s/FreeV2rays",
-    "https://t.me/s/v2ray_free_conf",
-    "https://t.me/s/PrivateVPNs",
-    "https://t.me/s/IP_CF_Config",
-    "https://t.me/s/prrofile_purple",
-    "https://t.me/s/meli_proxyy",
-    "https://t.me/s/DirectVPN",
-    "https://t.me/s/moftconfig",
-    "https://t.me/s/proxyiranip",
-    "https://t.me/s/v2nodes",
-    "https://t.me/s/grizzlyvpn",
-    "https://t.me/s/v2ray_extractor",
+    "https://t.me/s/ConfigsHubPlus",
+    "https://t.me/s/ShadowProxy66",
+    "https://t.me/s/Hope_Net",
     "https://t.me/s/v2ray_configs_pool",
-    "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/trojan",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/vless",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/vmess",
-    # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/shadowsocks",
+    #"https://t.me/s/IP_CF_Config",
+    #"https://t.me/s/prrofile_purple",
+    #"https://t.me/s/meli_proxyy",
+    #"https://t.me/s/DirectVPN",
+    #"https://t.me/s/moftconfig",
+    #"https://t.me/s/proxyiranip",
+    #"https://t.me/s/v2nodes",
+    #"https://t.me/s/grizzlyvpn",
+    #"https://t.me/s/v2ray_extractor",
+    #"https://t.me/s/v2ray_configs_pool",
+    #"https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
+     "https://raw.githubusercontent.com/alirezaazarnoosh/LOG/refs/heads/main/README.md",
+     "https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/mix/sub.html",
+     "https://raw.githubusercontent.com/alirezaazarnoosh/LOG/refs/heads/main/subshadmanegi",
+     "https://raw.githubusercontent.com/miladtahanian/V2RayCFGDumper/main/config.txt",
     # "https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/main/protocols/hysteria", 
-    "https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
+    #"https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt",
     # Add more URLs here if you want to include additional sources.
 ]
 
@@ -33,7 +34,7 @@ USE_MAXIMUM_POWER = False
 
 # Desired number of configurations to fetch.
 # This is used only if USE_MAXIMUM_POWER is False.
-SPECIFIC_CONFIG_COUNT = 300
+SPECIFIC_CONFIG_COUNT = 500
 
 # Dictionary of protocols to enable or disable.
 # Set each protocol to True to enable, False to disable.
@@ -41,10 +42,10 @@ ENABLED_PROTOCOLS = {
     "wireguard://": False,
     "hysteria2://": True,
     "vless://": True,
-    "vmess://": True,
-    "ss://": True,
-    "trojan://": True,
-    "tuic://": True,
+    "vmess://": False,
+    "ss://": False,
+    "trojan://": False,
+    "tuic://": False,
 }
 
 # Maximum age of configurations in days.
